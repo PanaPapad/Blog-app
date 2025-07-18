d0619608995f0e7b10c2faca8348842d7481e29b
Panagiotis <panagiotis---@live.com>
2025-07-19 00:58:25 +0300

    Genesis

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!Doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+</head>
+<body>
+    <div>Hello, Laravel!</div>
+</body>
+</html>
